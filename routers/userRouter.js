0a7b15ae8968515e0cc0267a13d4bbcf54ff21cd
이규원 <trq1.lee@gmail.com>
2019-07-08 16:58:10 +0900

    create mvc

---
+++ routers/userRouter.js
@@ -0,0 +1,12 @@
+import express from "express";
+import routes from "../routes";
+import { users, userDetail, editProfile, changePasswd } from "../controllers/userControllers";
+
+const userRouter = express.Router();
+
+userRouter.get(routes.users, users);
+userRouter.get(routes.userDetail, userDetail);
+userRouter.get(routes.editProfile, editProfile);
+userRouter.get(routes.changePassword, changePasswd);
+
+export default userRouter;
