ba935620c5a4f3b77de954de04ef885f49ddaddb
이규원 <trq1.lee@gmail.com>
2019-07-12 20:29:33 +0900

    add local variables in pug

--- app.js
+++ app.js
@@ -3,13 +3,15 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { localMiddleware } from "./middleware";
+import routes from "./routes";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
-import routes from "./routes";
 
 const app = express();
 
+app.use(helmet());
 /**
  * Setting pug
  */
@@ -21,9 +23,13 @@ app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser());
-app.use(helmet());
 app.use(morgan("dev"));
+
+/**
+ * local Variable
+ */
+
+app.use(localMiddleware)
 
 /**
  * Setting Router
